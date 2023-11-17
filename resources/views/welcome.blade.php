8234b004104415b730162121c189c9c5c3f8bca0
junaidmugloo <junaidmugloo@gmail.com>
2023-11-17 21:41:14 +0530

    foot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -593,11 +593,7 @@
         <!--**********************************
             Footer start
         ***********************************-->
-        <div class="footer">
-            <div class="copyright">
-                <p>Copyright &copy; Designed & Developed by <a href="https://themeforest.net/user/quixlab">Quixlab</a> 2018</p>
-            </div>
-        </div>
+        @include('components/footer');
         <!--**********************************
             Footer end
         ***********************************-->
